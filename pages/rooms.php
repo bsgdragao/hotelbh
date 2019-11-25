40c8601e0c9e500cec9731f3884f4b78fa5789a1
unknown <bsgdragao@gmail.com>
2019-11-25 00:04:04 -0300

    Room page inserted

---
+++ pages/rooms.php
@@ -0,0 +1,195 @@
+<title>Quartos</title>
+<form action="form-action.php" method="post">
+            <p>
+                <h3><u>Quarto 101</u> <br/> Estado de limpeza do quarto:</h3>
+                <input type="radio" name="limpeza101" value="quarto-101-limpo"/>Limpo
+                <input type="radio" name="limpeza101" value="quarto-101-sujo"/> Sujo
+                
+            </p>
+            <p>
+                    <h3>Disponibilidade:</h3>
+                    <input type="radio" name="quarto101" value="quarto-101-in"/>Ocupado (Check-in) 
+                    <input type="radio" name="quarto101" value="quarto-101-out"/>Livre (Check-out)
+                    
+            </p>
+            <p>
+                    <h3><u>Quarto 102</u> <br/> Estado de limpeza do quarto:</h3>
+                    <input type="radio" name="limpeza102" value="quarto-102-limpo"/>Limpo
+                    <input type="radio" name="limpeza102" value="quarto-102-sujo"/> Sujo
+                    
+                </p>
+                <p>
+                        <h3>Disponibilidade:</h3>
+                        <input type="radio" name="quarto102" value="quarto-102-in"/>Ocupado (Check-in) 
+                        <input type="radio" name="quarto102" value="quarto-102-out"/>Livre (Check-out)
+                        
+                </p>
+                <p>
+                        <h3><u>Quarto 103</u> <br/> Estado de limpeza do quarto:</h3>
+                        <input type="radio" name="limpeza103" value="quarto-103-limpo"/>Limpo
+                        <input type="radio" name="limpeza103" value="quarto-103-sujo"/> Sujo
+                        
+                    </p>
+                    <p>
+                            <h3>Disponibilidade:</h3>
+                            <input type="radio" name="quarto103" value="quarto-103-in"/>Ocupado (Check-in) 
+                            <input type="radio" name="quarto103" value="quarto-103-out"/>Livre (Check-out)
+                            
+                    </p>
+                    <p>
+                            <h3><u>Quarto 104</u> <br/> Estado de limpeza do quarto:</h3>
+                            <input type="radio" name="limpeza104" value="quarto-104-limpo"/>Limpo
+                            <input type="radio" name="limpeza104" value="quarto-104-sujo"/> Sujo
+                            
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto104" value="quarto-104-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto104" value="quarto-101-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 201</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza201" value="quarto-201-limpo"/>Limpo
+                                <input type="radio" name="limpeza201" value="quarto-201-sujo"/> Sujo
+                                
+                            </p>
+                            <p>
+                                    <h3>Disponibilidade:</h3>
+                                    <input type="radio" name="quarto201" value="quarto-201-in"/>Ocupado (Check-in) 
+                                    <input type="radio" name="quarto201" value="quarto-201-out"/>Livre (Check-out)
+                                    
+                            </p>
+                            <p>
+                                    <h3><u>Quarto 202</u> <br/> Estado de limpeza do quarto:</h3>
+                                    <input type="radio" name="limpeza202" value="quarto-202-limpo"/>Limpo
+                                    <input type="radio" name="limpeza202" value="quarto-202-sujo"/> Sujo
+                                    
+                                </p>
+                                <p>
+                                        <h3>Disponibilidade:</h3>
+                                        <input type="radio" name="quarto202" value="quarto-202-in"/>Ocupado (Check-in) 
+                                        <input type="radio" name="quarto202" value="quarto-202-out"/>Livre (Check-out)
+                                        
+                                </p>
+                        <p>
+                                <h3><u>Quarto 203</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza203" value="quarto-203-limpo"/>Limpo
+                                <input type="radio" name="limpeza203" value="quarto-203-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto203" value="quarto-203-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto203" value="quarto-203-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 204</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza204" value="quarto-204-limpo"/>Limpo
+                                <input type="radio" name="limpeza204" value="quarto-204-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto204" value="quarto-204-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto204" value="quarto-204-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 301</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza301" value="quarto-301-limpo"/>Limpo
+                                <input type="radio" name="limpeza301" value="quarto-301-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto301" value="quarto-301-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto301" value="quarto-301-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 302</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza302" value="quarto-302-limpo"/>Limpo
+                                <input type="radio" name="limpeza302" value="quarto-302-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto302" value="quarto-302-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto302" value="quarto-302-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 303</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza303" value="quarto-303-limpo"/>Limpo
+                                <input type="radio" name="limpeza303" value="quarto-303-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto303" value="quarto-303-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto303" value="quarto-303-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 304</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza304" value="quarto-304-limpo"/>Limpo
+                                <input type="radio" name="limpeza304" value="quarto-304-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto304" value="quarto-304-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto304" value="quarto-304-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 401</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza401" value="quarto-401-limpo"/>Limpo
+                                <input type="radio" name="limpeza401" value="quarto-401-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto401" value="quarto-401-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto401" value="quarto-401-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 402</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza402" value="quarto-402-limpo"/>Limpo
+                                <input type="radio" name="limpeza402" value="quarto-402-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto402" value="quarto-402-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto402" value="quarto-402-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 403</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza403" value="quarto-403-limpo"/>Limpo
+                                <input type="radio" name="limpeza403" value="quarto-403-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto403" value="quarto-403-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto403" value="quarto-403-out"/>Livre (Check-out)
+                                
+                        </p>
+                        <p>
+                                <h3><u>Quarto 404</u> <br/> Estado de limpeza do quarto:</h3>
+                                <input type="radio" name="limpeza404" value="quarto-404-limpo"/>Limpo
+                                <input type="radio" name="limpeza404" value="quarto-404-sujo"/> Sujo
+                                
+                        </p>
+                        <p>
+                                <h3>Disponibilidade:</h3>
+                                <input type="radio" name="quarto404" value="quarto-404-in"/>Ocupado (Check-in) 
+                                <input type="radio" name="quarto404" value="quarto-404-out"/>Livre (Check-out)
+                                
+                        </p>
+
